11632f4adf2d8a6a07240df6fd1d69daba259f43
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-11 23:20:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,9 +3,5 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    true
-  elsif board[index] == "X" || board[index] == "O"
-    false
-  end
+  board[index] == " " || board[index] == "" || board[index] == nil
 end
